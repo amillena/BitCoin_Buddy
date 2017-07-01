4d1f70691355638c142870812d93a051dfd355ea
amillena <amillena@yahoo.com>
2017-06-30 21:18:08 -0700

    updated configuration for mongoose.connection

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
-var configDB = require('./config/database.js');
+//var configDB = require('./config/database.js');
 
 var now = new Date();
 var start = '2016-08-01';
@@ -30,7 +30,28 @@ var end = '2016-08-25';
 
 // configuration ===============================================================
 //mongoose.connect(configDB.url); // connect to our local database
-mongoose.connect(process.env.MONGODB_URI); // connect to our MLAB
+
+
+//Database configuration with mongoose
+var databaseUri = "mongodb://localhost/passport";
+  if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+    }else{
+    mongoose.connect(databaseUri);
+  }
+
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
 
 require('./config/passport')(passport); // pass passport for configuration
 
